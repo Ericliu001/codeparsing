17b01ac7aa16e858e6a97e4cce986a58e3b32859
Eric Liu <eric.liu@uber.com>
2023-02-10 10:40:29 -0800

    parses a file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,23 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.com.intellij.openapi.util.Disposer
+import org.jetbrains.kotlin.com.intellij.psi.PsiFileFactory
+import org.jetbrains.kotlin.com.intellij.testFramework.LightVirtualFile
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.idea.KotlinLanguage
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val project = KotlinCoreEnvironment.createForProduction(
+        Disposer.newDisposable(),
+        CompilerConfiguration(),
+        EnvironmentConfigFiles.JVM_CONFIG_FILES
+    ).project
+
+    val file = PsiFileFactory.getInstance(project)
+        .createFileFromText(
+            KotlinLanguage.INSTANCE, "fun main() { println(\"Hello World!\") }"
+        )
+
+    println(file.text)
 }
-
