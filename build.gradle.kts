14f0fd0ea8b69073496441a8aa58b089e0616da5
Eric Liu <eric.liu@uber.com>
2023-02-28 17:42:17 -0800

    removes redundant build.gradle lines

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,25 +6,9 @@ plugins {
 
 repositories {
     mavenCentral()
-    jcenter()
-
-    maven {
-        url = uri("https://www.jetbrains.com/intellij-repository/releases/")
-    }
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.8.10")
-
-//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
-//    implementation("com.jetbrains.intellij.platform:uast:223.8214.52")
-//    implementation("com.jetbrains.intellij.java:java-uast:223.8214.52")
-//    runtimeOnly("com.jetbrains.intellij.java:java-psi:223.8214.52")
-//    runtimeOnly("com.jetbrains.intellij.java:java-psi-impl:223.8214.52")
-//    runtimeOnly("com.jetbrains.intellij.java:java-analysis-impl:223.8214.52")
-//    runtimeOnly("com.jetbrains.intellij.java:java-impl:223.8214.52")
-//    runtimeOnly("com.jetbrains.intellij.java:java-psi-impl:223.8214.52")
-
-
     testImplementation(kotlin("test"))
 }
